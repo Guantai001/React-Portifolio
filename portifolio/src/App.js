f928c552b9dc7248436409f329b5b0cda92bc0d8
guantai <jpguantai@gmail.com>
2023-02-02 13:47:54 +0300

    works

--- portifolio/src/App.js
+++ portifolio/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import NavBar from './components/NavBAr';
 import Home from './components/Home';
 import About from './components/About';
+import Services from './components/Services';
+import Work from './components/Work';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <NavBar/>
       <Home/>
       <About/>
+      <Services/>
+      <Work/>
     </div>
   );
 }
